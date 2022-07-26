9384d2367c6ddec4c82addd8877570824bed2dd7
jahedripon9 <jahedahmed3170@gmail.com>
2022-07-26 23:30:16 +0600

    Complete Project

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import { Typography } from "@mui/material";
+import React, { FC, ChangeEvent, useState } from "react";
+import "./App.css";
+import TodoTask from "./Components/TodoTask";
+import { ITask } from "./Interfaces";
+
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDealine] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDealine(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline };
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setDealine(0);
+  };
+
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName != taskNameToDelete;
+      })
+    );
+  };
+
+  return (
+    <div className="App">
+           <Typography style={{marginTop:'20px', backgroundColor:'#FF6347', padding:20, borderRadius:'20px'}} variant="h3" gutterBottom component="div">
+        Create Todo List
+      </Typography>
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="Task..."
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Deadline (in Days)..."
+            name="deadline"
+            value={deadline}
+            onChange={handleChange}
+          />
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="todoList">
+        {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/TodoTask.tsx
@@ -0,0 +1,28 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { ITask } from "../Interfaces";
+
+interface Props {
+  task: ITask;
+  completeTask(taskNameToDelete: string): void;
+}
+
+const TodoTask = ({ task, completeTask }: Props) => {
+  return (
+    <Box className="task">
+      <div className="content">
+        <span>{task.taskName}</span>
+        <span>{task.deadline}</span>
+      </div>
+      <button
+        onClick={() => {
+          completeTask(task.taskName);
+        }}
+      >
+        X
+      </button>
+    </Box>
+  );
+};
+
+export default TodoTask;
